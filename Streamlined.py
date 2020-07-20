198f9d0f7b95bdb35e0c055d3bd8b19651eded22
aasibipin <aasibipin@gmail.com>
2020-07-19 20:01:40 -0400

    Converting to Object Oriented

--- Streamlined.py
+++ Streamlined.py
@@ -8,6 +8,26 @@ import time
 import pandas as pd
 from defs import EuclidDistance
 
+class Target: 
+    def __init__(self, img):
+        self.image = img
+        self.greyImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        self.width, self.height = self.greyImage.shape[::-1]
+        self.yList = []
+        self.xList = []
+
+    def addCoordinates(self,x,y):
+        self.xCoordinate = x
+        self.yCoordinate = y        
+        self.xList.append(x)
+        self.yList.append(y)
+
+
+    def DrawOnFrame (self, frame, borderSize, color = (0,255,0)):
+        cv2.rectangle(frame, (self.xCoordinate,self.yCoordinate), 
+        (self.xCoordinate + self.width, self.yCoordinate + self.height) ,color,borderSize)
+
+
 ###### PLOTTING ############# 
 X_AXISLIM = (0,650)
 Y_AXISLIM = (0,400)
@@ -22,51 +42,38 @@ ax1.grid(True)
 
 cap = cv2.VideoCapture(0)
 
-template = cv2.imread('#.png')
-template_gray = cv2.cvtColor(template,cv2.COLOR_BGR2GRAY)
+hashtag = cv2.imread('#.png')
+template = Target(img=hashtag)
 
-template2 = cv2.imread('!.png')
-template_gray2 = cv2.cvtColor(template2,cv2.COLOR_BGR2GRAY)
+template2 = Target(img= cv2.imread('!.png'))
 
-w,h = template_gray.shape[::-1]
-w2,h2 = template_gray2.shape[::-1]
-
-X = []
-Y = []
-X2 = []
-Y2 = []
 t = []
 start = round(time.perf_counter(),1)
 t.append(0)
 BORDER = 2
+THRES = 0.8
+
 while True:
     # cv2.waitKey(20)
     _, frame = cap.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     t.append(round(time.perf_counter(),1)-start)
 
-    res = cv2.matchTemplate(gray_frame, template_gray, cv2.TM_CCOEFF_NORMED)   #TRY DIFFERENT METHODS
-    THRES = 0.9
-    loc = np.where(res>=THRES)
-    Ys = loc[0].tolist()
-    Xs = loc[1].tolist()
+    results = cv2.matchTemplate(gray_frame, template.greyImage, cv2.TM_CCOEFF_NORMED)   #TRY DIFFERENT METHODS
+    locations = np.where(results>=THRES)
+    Ys = locations[0].tolist()
+    Xs = locations[1].tolist()
     if len(Xs) != 0:           # Can test either Xs or Ys, ensuring that object is detected
-        x = Xs[-1]
-        y = Ys[-1]
-        X.append(x)
-        Y.append(y)
-        cv2.rectangle(frame, (x,y), (x + w, y + h), (0,255,0),BORDER)
+        template.addCoordinates(x = Xs[-1], y = Ys[-1])
+        template.DrawOnFrame(frame=frame,borderSize= BORDER)
 
-    res = cv2.matchTemplate(gray_frame, template_gray2, cv2.TM_CCOEFF_NORMED)   #TRY DIFFERENT METHODS
-    loc2 = np.where(res>=THRES)
-    Y2s = loc2[0].tolist()
-    X2s = loc2[1].tolist() 
+    results2 = cv2.matchTemplate(gray_frame, template2.greyImage, cv2.TM_CCOEFF_NORMED)   #TRY DIFFERENT METHODS
+    locations2 = np.where(results2>=THRES)
+    Y2s = locations2[0].tolist()
+    X2s = locations2[1].tolist() 
     if len(X2s) != 0:           
-        x2 = X2s[-1]
-        y2 = Y2s[-1]
-        X2.append(x2)
-        Y2.append(y2)
-        cv2.rectangle(frame, (x2,y2) , (x2 + w2, y2 + h2), (255,0,0),BORDER)
+        template2.addCoordinates(x = X2s[-1], y = Y2s[-1])
+        template2.DrawOnFrame(frame=frame,borderSize= BORDER, color= (255,255,0))
 
     cv2.imshow("frame",frame)
 
@@ -75,16 +82,14 @@ while True:
 
 
 PHYSICALDISTANCE = 4
-avg_dist = EuclidDistance(X,Y,X2,Y2)
+avg_dist = EuclidDistance(template.xList, template.yList, template2.xList, template2.yList)
 print("Average Distance: ", avg_dist)
 print("Time of reading: ", t[-1])
 print('CM / Pixel: %.2f' %(PHYSICALDISTANCE/avg_dist))
 
 
-ax1.plot(X,Y, color='r')
-ax1.plot(X2,Y2, color='b')
-
-    
+ax1.plot(template.xList,template.yList, color='r')
+ax1.plot(template2.xList,template2.yList, color='b')
 
  
 plt.tight_layout
@@ -92,3 +97,4 @@ plt.show()
 
 cv2.destroyAllWindows()
 cap.release()
+
